fe35b8ca53b188356c9e8f1fb4d01f5c8ca54722
ErykSikora <eryksikora95@gmail.com>
2020-09-30 09:18:56 +0200

    0.2 working file saving

--- function.optimizer.php
+++ function.optimizer.php
@@ -5,7 +5,7 @@ function smarty_function_optimizer($params, &$smarty){
     //config
     $optimizer = [];
     empty($params['type']) ?            $optimizer['type'] = 'image' :              $optimizer['type'] = $params['type'];
-    empty($params['file']) ?            $validation = false :                       $optimizer['file'] = $params['file'];
+    empty($params['src']) ?             $validation = false :                       $optimizer['src'] = $params['src'];
     empty($params['assign']) ?          $optimizer['assign'] = false :              $optimizer['assign'] = $params['assign'];
     empty($params['width']) ?           $optimizer['width'] = 360 :                 $optimizer['width'] = (int) $params['width'];
     empty($params['height']) ?          $optimizer['height'] = 480 :                $optimizer['height'] = (int) $params['height'];
@@ -21,7 +21,8 @@ function smarty_function_optimizer($params, &$smarty){
     // const
     $allowed_extensions = ['jpg', 'jpeg', 'png'];
     
-    // functions
+    # --- Functions
+    
     function getFileFormat($file, $allowed){
         $pretend_format = end(explode('.',$file));
         if (in_array($pretend_format, $allowed)) {
@@ -31,22 +32,55 @@ function smarty_function_optimizer($params, &$smarty){
         }
     }
 
+    function createImage($src, $dir, $filename, $width, $height, $quality) {
+        
+        $image = @imagecreatefromjpeg($src);
+        $file = explode(".",$filename);
+        
+        # check if $file contains two elements (name and extension)
+        if (count($file) == 2 && $image) {
+
+            $image_width = imagesx($image);
+            $image_height = imagesy($image);
+            $size = image_crop_type($width, $height, $image_width, $image_height);
+            $thumb = imagecreatetruecolor($width, $height);
+            $newFilename = $file[0].'-'.$width.'-'.$height.'.'.$file[1];
+            echo $newFilename;
+            # resize and crop
+            imagecopyresampled($thumb, $image, 0 - $size['offset_width'] / 2, 0 - $size['offset_height'] / 2, 0, 0, $size['scale_width'], $size['scale_height'], $image_width, $image_height);
+            imagejpeg($thumb, $dir.'/'.$newFilename, $quality);
+            echo $dir.$newFilename;
+
+        } else {
+            return false;
+        }
+
+    }
+
+    # --- Mechanics
+
     try {
 
-        if (!file_exists($optimizer['file'])) throw new Exception('File not found', 100); #FIXME: 
+        if (!file_exists($optimizer['src'])) throw new Exception('File not found', 100); #FIXME: 
+        #TODO: If overwrited file exits return this link
 
         # file correcting if starts with '/'
-        if (strpos($optimizer['file'], '/') == 0) $optimizer['file'] = substr($optimizer['file'], 1);
+        if (strpos($optimizer['src'], '/') == 0) $optimizer['src'] = substr($optimizer['src'], 1);
+        
+        # get file name (with ext)
+        $optimizer['file'] = end(explode('/', $optimizer['src']));
 
         # getting file format
-        $ext = getFileFormat($optimizer['file'], $allowed_extensions);
+        $ext = getFileFormat($optimizer['src'], $allowed_extensions);
         if (!$ext) throw new Exception('Invalid file extension', 101);
 
         #TODO: File mechanics
 
+        createImage($optimizer['src'], $optimizer['dir'], $optimizer['file'], $optimizer['width'], $optimizer['height'], $optimizer['quality']);
+
     } catch (Exception $e) {
 
-        echo 'OPTIMIZER PLUGIN ERROR ['.$e->getMessage().'::'.$e->getCode().']';
+        echo 'OPTIMIZER PLUGIN ERROR ['.$e->getCode().']: '.$e->getMessage();
 
     }
 
